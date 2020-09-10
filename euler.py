fc949d951910262660e6c9b4e6578cccacbbdaac
Diego <ldrc161997@gmail.com>
2020-09-09 22:45:46 -0600

    funciones de tarea corta numero 1 creadas

--- euler.py
+++ euler.py
@@ -1,8 +1,15 @@
 def e_cuadratica(n):
     # Implemente esta función
-    return 0
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1 
+    else return ((1/factorial(e_cuadratica(n)))+((1/factorial(e_cuadratica(n-1)))
 
 
 def e_lineal(n):
     # Implemente esta función
-    return 0
+    e = 1
+    for i in range(0,n):
+    	e = e + (1/factorial(n))
+    return e
